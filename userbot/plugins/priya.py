51dac8d1e2e9891ad9d7348d0774daca149bd241
QUEENPRIYAOP <95538576+QUEENPRIYAOP@users.noreply.github.com>
2022-02-06 16:55:51 +0530

    Create priya.py

---
+++ userbot/plugins/priya.py
@@ -0,0 +1,13 @@
+# PLUGIN MADE BY @MAFIARISHABH FOR THANOSBOT 
+# KEEP CREDITS ELSE GAY
+
+import random, re
+from REBELBOT.utils import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="priya ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("""I HAVE A BF MAIN BASS RISHABH SE PYAAR KARTI HU. WO BASS MERA HAI💞💕💗 OUR MAIN SERP RISHABH KI HU 💖. I LOVE 💝YOU RISHABH😘😘""")
+
